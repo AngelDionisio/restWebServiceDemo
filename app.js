a7475faac144f4cf427b83b3f023479eca383cec
adionisiodr <adionisio82@gmail.com>
2015-08-21 20:57:08 -0400

    saving post data to mongodb

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ bookRouter.route('/Books')
 	.post(function (req, res) {
 		var book = new Book(req.body);
 
-		console.log(book);
-		res.send(book);
+		console.log('Saving book: ', book);
+		book.save();
+		res.status(201).send(book);
 
 	})
 	.get(function (req, res) {
